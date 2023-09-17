079e88e4e00267d8d0c4a0efb5b570286105d96d
luis <luis111ferrivera@gmail.com>
2023-09-17 16:39:56 -0400

    3ra Version

--- assets/js/components/darkMode.js
+++ assets/js/components/darkMode.js
@@ -1,7 +1,29 @@
+const ls = window.localStorage
 const containerDark = document.getElementById('toggle')
+
+if(ls.getItem('darkMode')){
+    document.body.classList.add('dark')
+    containerDark.classList.add('bxs-sun')
+}else{
+    document.body.classList.remove('dark')
+    containerDark.classList.add('bxs-moon')
+}
+
 function darkMode(){
     containerDark.addEventListener('click',function(event){
+
             document.body.classList.toggle('dark')
+
+            if(document.body.classList.contains('dark')){
+                ls.setItem('darkMode','dark')
+                containerDark.classList.add('bxs-sun')
+                containerDark.classList.remove('bxs-moon')
+            }else{
+                ls.removeItem('darkMode')
+                containerDark.classList.add('bxs-moon')
+                containerDark.classList.remove('bxs-sun')
+                
+            }
     })
 }
 
